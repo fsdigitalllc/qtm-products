f8eea4e75a6089a2c28a2dd59ecca8cad8dfbee2
Ben Bozzay <ben@fullstackdigital.com>
2019-02-07 12:19:57 -0500

    added existing qtm scripts to check for compatibility issues

---
+++ gulp/paths/css.vendor.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = [
+  './node_modules/normalize.css/normalize.css',
+  './node_modules/aos/dist/aos.css',
+  './node_modules/owl.carousel/dist/assets/owl.carousel.css',
+  './node_modules/owl.carousel/dist/assets/owl.theme.default.css'
+];
