e8c3657e2a35141faa1a7a80337a1964cabadeac
Ben Bozzay <ben@fullstackdigital.com>
2019-02-22 10:16:55 -0500

    added quantum font styles to menu

--- 222/old/gulp/paths/css.vendor.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = [
-  './node_modules/normalize.css/normalize.css',
-  './node_modules/aos/dist/aos.css',
-  './node_modules/owl.carousel/dist/assets/owl.carousel.css',
-  './node_modules/owl.carousel/dist/assets/owl.theme.default.css'
-];
